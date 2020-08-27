efec1b92d30fda744c60b67e454fd4dc7860069b
lns13301 <rkdrbgks22@ks.ac.kr>
2020-08-27 12:09:28 +0900

    feat: Add member entity
    - member 엔티티 생성
    - 데이터베이스에 적용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,6 +11,19 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager entityManager = emf.createEntityManager();
+
+        // 데이터를 변경하는 모든 작업은 트랜잭션 안에서 해야한다.
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Member member = new Member();
+        member.setId(2L);
+        member.setName("HelloB");
+
+        entityManager.persist(member);
+
+        entityTransaction.commit();
+
 
         entityManager.close();
 
