1baa22f8ccfb0101aa0d64f446e012d86da54089
lns13301 <rkdrbgks22@ks.ac.kr>
2020-08-31 16:40:18 +0900

    feat: Change Member data
    - Member 클래스의 멤버 변수 변경

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,8 +1,7 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
@@ -10,30 +9,28 @@ public class Member {
     @Id
     private Long id;
 
-    private String name;
+    @Column(name = "name")
+    private  String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    // 기본값 ORDINAL 인데, 그냥 STRING 을 쓰도록 하자!
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // 최신 버전 부터는 @Temporal 사용할 필요 없이 LocalDate, LocalDateTime 을 사용하면 됨
+
+    @Lob
+    private String description;
 
     // JPA는 기본적으로 내부적으로 reflection 같은걸 사용하기 때문에 동적으로 객체를 생성할 수 있어야 해서 기본 생성자가 필요함
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
